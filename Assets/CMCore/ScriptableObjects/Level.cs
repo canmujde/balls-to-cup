656fc6470158a708f2eef31bbd7f033c83081ddd
canmujde <43178831+canmujde@users.noreply.github.com>
2023-12-21 01:26:32 +0300

    Update Level.cs

--- Assets/CMCore/ScriptableObjects/Level.cs
+++ Assets/CMCore/ScriptableObjects/Level.cs
@@ -1,15 +1,16 @@
 
+using Sirenix.OdinInspector;
 using UnityEngine;
-#if UNITY_EDITOR
-#endif
+using Object = UnityEngine.Object;
 
 namespace CMCore.ScriptableObjects
 {
     [CreateAssetMenu(fileName = "Level", menuName = "CMCore/Level")]
     public class Level : ScriptableObject
     {
-        
-        
+        [field: SerializeField, Required] public Object TubeSvg { get; private set; }
+         
+         
     }
 
 }
