c4c721b523538e3a5848425ad466d5ac8a2f9a52
canmujde <43178831+canmujde@users.noreply.github.com>
2023-12-23 09:32:20 +0300

    Update GameplaySettings.cs

--- Assets/CMCore/ScriptableObjects/GameplaySettings.cs
+++ Assets/CMCore/ScriptableObjects/GameplaySettings.cs
@@ -8,6 +8,20 @@ namespace CMCore.ScriptableObjects
     [CreateAssetMenu(fileName = "GameplaySettings", menuName = "CMCore/Gameplay Settings")]
     public class GameplaySettings : ScriptableObject
     {
+        [field: SerializeField] public float RotationSensitivity { get; private set; }
+        
+        [field: SerializeField, Range(0,1)] public float BallBounciness { get; private set; } 
+        [field: SerializeField, Range(0,100)] public float BallFriction { get; private set; }
+        [field: SerializeField, Range(0,100)] public float BallAirFriction { get; private set; }
+        
+        [field: SerializeField] public PhysicMaterial BallPhysicMaterial { get; private set; }
+
+        private void OnValidate()
+        {
+            if (BallPhysicMaterial==null) return;
+            BallPhysicMaterial.bounciness = BallBounciness;
+            BallPhysicMaterial.dynamicFriction = BallPhysicMaterial.staticFriction = BallFriction;
+        }
     }
     
 }
