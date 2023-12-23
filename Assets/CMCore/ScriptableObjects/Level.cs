36f06c74e0b1732db9145e2839f4f7e24788ba17
canmujde <43178831+canmujde@users.noreply.github.com>
2023-12-23 09:32:22 +0300

    Update Level.cs

--- Assets/CMCore/ScriptableObjects/Level.cs
+++ Assets/CMCore/ScriptableObjects/Level.cs
@@ -1,4 +1,5 @@
 
+using CMCore.Models;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -8,7 +9,11 @@ namespace CMCore.ScriptableObjects
     [CreateAssetMenu(fileName = "Level", menuName = "CMCore/Level")]
     public class Level : ScriptableObject
     {
-        [field: SerializeField, Required] public Object TubeSvg { get; private set; }
+        [field: SerializeField] public Object TubeSvg { get; private set; }
+        [field: SerializeField, Range(-3f, 3f)] public float SvgTubeExtraXOffset { get; private set; }
+        [field: SerializeField] public StaticTube PreMeshTube { get; private set; }
+        [field: SerializeField, Range(20,100)] public int BallCount { get; private set; }
+        [field: SerializeField, Range(0.15f,1)] public float BallScale { get; private set; }
          
          
     }
